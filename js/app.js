466b206000ee166b5cd4b5e34239e5ba2fbb2c6b
itano11 <atambur@gmail.com>
2022-06-14 09:09:26 -0400

    version 1.0.0

---
+++ js/app.js
@@ -0,0 +1,24 @@
+
+fetch("https://api.nasa.gov/planetary/apod?api_key=FRcaIi60mo1ho0btxsCy2idKcp6DIilfzkLlkHbV")
+
+.then(response => {return response.json() })
+.then(data => {
+     console.log(data)
+
+     let title = data.title
+     let image = data.url
+     let description = data.explanation
+     let date = data.date
+     let copyr = data.copyright
+
+     titulo.textContent = `${title}`
+     fecha.textContent = `${date}`
+     descripcion.textContent = `${description}`
+     copy.textContent = `${copyr}`
+
+     const tarjeta =`<div class="pt-3 text-center">
+     <img src="${image}" class="img-fluid" alt="image"/></div>`
+
+     document.getElementById('theimage').innerHTML += tarjeta
+})
+.catch(err => console.error(err));
